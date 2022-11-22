a2867e157aaba8e04f72b87b4f2f7d86d7136efd
Dianisa <dianisawulandari@gmail.com>
2022-11-22 13:42:18 +0700

    Progress Transaksi Pelanggan

--- KategoriMakanan/urls.py
+++ KategoriMakanan/urls.py
@@ -4,7 +4,7 @@ from KategoriMakanan.views import *
 app_name = 'KategoriMakanan'
 
 urlpatterns = [
-    path('FormKategoriMakanan/', show_form_kategori_makanan, name='show_form_kategori_makanan'),
-    path('DaftarKategoriMakanan/', show_daftar_kategori_makanan, name='show_daftar_kategori_makanan'),
-    path('HapusKategoriMakanan/', hapus_kategori_makanan, name='hapus_kategori_makanan'),
+    path('Form/', show_form_kategori_makanan, name='show_form_kategori_makanan'),
+    path('Daftar/', show_daftar_kategori_makanan, name='show_daftar_kategori_makanan'),
+    path('Hapus/', hapus_kategori_makanan, name='hapus_kategori_makanan'),
 ]
