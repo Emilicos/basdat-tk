ceaac62a93248fc25ea2f6467b37ddb1a41603ed
alvaro.austin <alvaro.austin@ui.ac.id>
2022-12-07 14:26:30 +0700

    finish tarif

--- pengiriman_tarif_per_km/urls.py
+++ pengiriman_tarif_per_km/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from pengiriman_tarif_per_km.views import show_tarif, create_tarif
+from pengiriman_tarif_per_km.views import show_tarif, show_create_tarif, delete_tarif, update_tarif
 app_name = "tarif"
 
 urlpatterns = [
-    path('create/', create_tarif, name = 'create_tarif'),
+    path('create/', show_create_tarif, name = 'create_tarif'),
+    path('delete/<str:id>/', delete_tarif, name = 'delete_tarif'),
     path('', show_tarif, name = 'show_tarif'),
+    path('update/', update_tarif, name = 'update_tarif'),
 ]
