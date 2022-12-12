1b1d1bf3792aebff76d86eda2ffa703a50202fa0
Dianisa <dianisawulandari@gmail.com>
2022-12-12 21:57:05 +0700

    benerin views

--- authentication/views.py
+++ authentication/views.py
@@ -332,31 +332,5 @@ def verification(request, email):
             """)
     return HttpResponse()
 
-def detail_restoran(request):
-    context = {
-        'user': {
-            'role': 'Admin',
-        }
-    }
-    return render(request, 'dashboard_restoran.html', context)
-
-def detail_kurir(request):
-    context = {
-        'user': {
-            'role': 'Admin',
-        }
-    }
-    return render(request, 'dashboard_kurir.html', context)
-
-from django.core import serializers
-def all_user(request):
-    with connection.cursor() as cursor:
-        cursor.execute('SET SEARCH_PATH TO SIREST;')
-        cursor.execute(f'''
-            SELECT *
-            FROM USER_ACC;
-        ''')
-        user_list = dict_fetch_all(cursor)
-    return HttpResponse(serializers.serialize("json", user_list), content_type="application/json")
 
 
