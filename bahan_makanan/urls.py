bac677b68c067e8f0882b7c25d521a7d7cf24808
Fauzan Daffa R <daffa.giis@gmail.com>
2022-12-13 23:23:57 +0700

    fix bahan makanan

--- bahan_makanan/urls.py
+++ bahan_makanan/urls.py
@@ -5,6 +5,6 @@ app_name = 'bahan_makanan'
 
 urlpatterns = [
     path('form/', show_form_bahan_makanan, name='show_form_makanan'),
-    path('daftar/', show_form_bahan_makanan, name='show_form_bahan_makanan'),
-    path('hapus/', hapus_bahan_makanan, name='hapus_bahan_makanan'),
+    path('daftar/', show_daftar_bahan_makanan, name='show_form_daftar_makanan'),
+    path('hapus/<str:id>/', hapus_bahan_makanan, name='hapus_bahan_makanan'),
 ]
--- bahan_makanan/views.py
+++ bahan_makanan/views.py
@@ -1,7 +1,7 @@
 import datetime
 from distutils.command.clean import clean
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
@@ -11,67 +11,8 @@ from django.db import connection
 from datetime import datetime
 import random
 import string
+from django.views.decorators.csrf import csrf_exempt
 
-kursor = connection.cursor()
-
-@login_required
-def show_daftar_bahan_makanan(request):
-    if request.session.get("role") != "admin":
-        return HttpResponse("Anda tidak mmemiliki akses ke halaman ini")
-    kursor.execute("SELECT * FROM x.ingredient")
-    ingredients = kursor.fetchall()
-    ingredients_fix = []
-    for ingredient in ingredients:
-        id = ingredients[0]
-        name = ingredients[1]
-        kursor.execute("SELECT * FROM x.food_ingredient WHERE ingredient = %s", [id])
-        ingredient = {
-            "id" : id,
-            "name" : name, 
-            "is_referenced" : kursor.fetchone() is not None,
-        }
-        ingredients_fix.append(ingredient)
-    return render(
-        request, "bahan_makanan.html", {"ingredients" : ingredients_fix}
-    )
-
-@login_required
-def show_form_bahan_makanan(request):
-    if request.session.get("role") != "admin":
-        return HttpResponse("Anda tidak memiliki akses ke halaman ini")
-    if request.method == "POST":
-        name = request.POST.get("name")
-        if name is None or name =="":
-            messages.error(request, "Data yang diisikan belum lengkap, silakan lengkapi data terlebih dahulu.")
-            return render(request, "form_bahan_makanan.html")
-        
-        kursor.execute("SELECT MAX(CAST(id AS INT)) FROM x.ingredient")
-        id = kursor.fetchone()
-        if id is None :
-            id = "1"
-        else:
-            id = str(int(id[0]) + 1)
-        try:
-            kursor.execute("INSERT INTO x.ingredient(id, name) VALUES (%s, %s)",[id, name],
-            )
-            messages.success(request, "Berhasil menambahkan bahan makanan baru.")
-            return redirect("bahan_makanan:show_daftar_bahan_makanan")
-        except Exception as e:
-            messages.error(request, e)
-    return render(request, "form_bahan_makanan.html")
-
-@login_required
-def hapus_bahan_makanan(request, id):
-    if request.session.get("role") != "admin":
-        return HttpResponse("Anda tidak messages akses ke halaman ini")
-    if request.method == "POST":
-        try:
-            kursor.execute("DELETE FROM x.ingredient WHERE id = %s", [id])
-            messages.success(request,"Berhasil menghapus bahan makanan")
-        except Exception as e:
-            messages.errorMessage(request, "Gagal menghapus bahan makanan")
-        return redirect("bahan_makanan:show_daftar_bahan_makanan")
-    return HttpResponse("Anda tidak messages akses ke halaman")
 
 def dictfetchall(cursor): 
     "Returns all rows from a cursor as a dict" 
@@ -81,13 +22,22 @@ def dictfetchall(cursor):
             for row in cursor.fetchall() 
     ]
 
+@csrf_exempt
 def show_form_bahan_makanan(request):
     if request.method == 'POST':
         namaBahan = request.POST.get('namaBahan')
-        if namaBahan != "":
-            return redirect('KategoriMakanan:show_daftar_bahan_makanan')
-        else:
-            messages.info(request, 'Data yang diisikan belum lengkap, silakan lengkapi data terlebih dahulu.')
+        print(namaBahan)
+        id = "i" + f"{random.randint(1, 1000000)}"
+        with connection.cursor() as cursor:
+                cursor.execute("SET SEARCH_PATH TO SIREST;")
+                cursor.execute(f"""
+                INSERT INTO INGREDIENT VALUES
+                ('{id}', '{namaBahan}');
+                 """)
+                cursor.execute("SET SEARCH_PATH TO PUBLIC;")
+        return JsonResponse({
+                    "message": "Successful"
+                })
     context = {
         'user': {
             'role': 'Admin',
@@ -102,31 +52,43 @@ def show_daftar_bahan_makanan(request):
             'user': {'role': 'Admin'}
         }
 
+        
         cursor.execute("SET SEARCH_PATH TO SIREST;")
         cursor.execute(f"""
-            SELECT rname, rbranch, foodname, ingredient
-            FROM FOOD_INGREDIENT;
+            SELECT *
+            FROM INGREDIENT;
         """)
 
         bahan = dictfetchall(cursor)
         context['bahan'] = bahan
 
-        # for i in range(len(kategori)):
-        #     context['kategori'][i]['nomor'] = str(i+1)
 
         cursor.execute("SET SEARCH_PATH TO PUBLIC")
+        for item in bahan :
+                cursor.execute("SET SEARCH_PATH TO SIREST;")
+                cursor.execute(f"""
+                        SELECT *
+                        FROM FOOD_INGREDIENT
+                        WHERE INGREDIENT = '{item['id']}';
+                    """)
+                transaction_food_data = dictfetchall(cursor)
+                if(len(transaction_food_data) > 0):
+                    item['referred'] = True
+                else:
+                    item['referred'] = False
+
 
     return render(request, 'bahan_makanan.html', context)
 
-def hapus_bahan_makanan(request): # TODO syntax delete makanan dengan if variable di branch FOOD_INGREDIENT dia equals / viable dengan yg ada di FOOD db
+def hapus_bahan_makanan(request, id): # TODO syntax delete makanan dengan if variable di branch FOOD_INGREDIENT dia equals / viable dengan yg ada di FOOD db
 #kalo udah di refer sama makanan baru gabisa dihapus
     with connection.cursor() as cursor:
         cursor.execute("SET SEARCH_PATH TO SIREST;")
         cursor.execute(f"""
-            DELETE FROM FOOD_INGREDIENT
-            WHERE rname={rname};
+            DELETE FROM INGREDIENT
+            WHERE id='{id}';
         """)
         cursor.execute("SET SEARCH_PATH TO PUBLIC")
 
     messages.info(request, 'Berhasil menghapus bahan makanan!')
-    return redirect('bahan_makanan:show_daftar_bahan_makanan')
+    return redirect('bahan_makanan:show_form_daftar_makanan')
